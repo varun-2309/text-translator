e0364f7aac7e6f1479873c9fa5b5483d97f9d069
varun <varunreniwal2309@gmail.com>
2025-05-10 08:21:02 -0400

    Initial commit - Flask Text Translator

---
+++ app.py
@@ -0,0 +1,21 @@
+from flask import Flask, request, jsonify, render_template
+from googletrans import Translator
+from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app)
+
+translator = Translator()
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/translate', methods=['POST'])
+def translate():
+    data = request.json
+    translated = translator.translate(data['text'], src=data['source'], dest=data['target'])
+    return jsonify({"translatedText": translated.text})
+
+if __name__ == '__main__':
+    app.run(debug=True)
